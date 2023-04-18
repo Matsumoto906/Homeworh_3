fa35631803fd97a8b424657cad3aae2ab397afa2
unknown <aaaaa09906@gmail.com>
2023-04-18 23:51:25 +0800

    4B1G0097

--- ITSA16/ITSA16.cpp
+++ ITSA16/ITSA16.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-double ans=0;
-int sum(double r,double n,double p){
-    ans+=(ans+p)*r+p;
-    n--;
-    if (n == 0)
-        return ans;
-    else    
-        return sum(r,n,p);
-}
-
-int main(){
-
-    double r;
-    int n,p;
-    cin >> r;
-    cin >> n;
-    cin >> p;
-    sum(r/100,n,p);
-    
-    cout << floor(ans) << endl;
-
-    return 0;
-}
+#include<iostream> 
+#include<string> 
+using namespace std; 
+ 
+int main() 
+{ 
+ string str1,str2; 
+    getline(cin,str1);  
+    getline(cin,str2); 
+    int i = 0,count = 0; 
+    while (true) 
+    { 
+     i = str2.find(str1,i); 
+     if (i != string::npos) 
+     { 
+          i++; 
+           count++; 
+       } 
+      else 
+        { 
+            break; 
+        } 
+    } 
+  cout << count << endl; 
+ return 0; 
+}  
